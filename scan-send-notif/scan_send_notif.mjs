2feba4b0f349814ae5b5f299e270d4a5d28a39b6
HoustonBoston <myhorsefly12345@gmail.com>
2024-12-21 10:37:13 -0500

    Add logic to verify only new users for SES.

--- scan-send-notif/scan_send_notif.mjs
+++ scan-send-notif/scan_send_notif.mjs
@@ -1,6 +1,5 @@
 import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb"
-import { PublishCommand, SNSClient } from "@aws-sdk/client-sns"
-import { SubscribeCommand } from "@aws-sdk/client-sns"
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses"
 
 import dayjs from "dayjs"
 import utc from 'dayjs/plugin/utc.js'
@@ -8,13 +7,14 @@ import timezone from 'dayjs/plugin/timezone.js'
 
 const client = new DynamoDBClient({})
 const tableName = "Item"
-const snsClient = new SNSClient({})
+const sesClient = new SESClient({})
 
 /**
  * Scans entire table, stores results in a hashmap, and sends SNS notif to appropriate emails if item expiration is a day or less
  */
 
-export const handler = async () => {
+export const handler = async () =>
+{
     console.log('entering scan_send_notif')
 
     try {
@@ -38,7 +38,8 @@ export const handler = async () => {
         const items = output.Items
         const expiringItemsMap = new Map()
 
-        items.forEach(item => {
+        items.forEach(item =>
+        {
             console.log('item:', item)
             console.log('item.item_name', item.item_name.S)
             const userEmail = item.user_email.S
@@ -60,46 +61,45 @@ export const handler = async () => {
             }
         })
 
-        // SNS
+        // SES
 
-        for (let [email, itemsArray] of expiringItemsMap) {
-            console.log('items array length:', itemsArray.length)
-            console.log('items array', itemsArray)
-            if (itemsArray.length > 0) {
+        for (let [email, expiringItemsArray] of expiringItemsMap) {
+            console.log('items array length:', expiringItemsArray.length)
+            console.log('expiring items array', expiringItemsArray)
+            if (expiringItemsArray.length > 0) {
                 console.log('user email in map:', email)
 
                 let message = `Dear user,\n\n The following items are expiring soon: \n\n` +
-                    itemsArray.map(item => {
+                    expiringItemsArray.map(item =>
+                    {
                         return `-${item}\n`
                     })
-
-                // const input = {
-                //     TopicArn: "arn:aws:sns:us-east-1:986376103464:ExpiringItems",
-                //     Protocol: "email",
-                //     Endpoint: email,
-                // }
-
-                // try {
-                //     const subRes = await snsClient.send(new SubscribeCommand(input))
-                //     console.log('snsRes:', subRes)
-                // } catch (error) {
-                //     console.error('SNS error:', error)
-                //     return {
-                //         statusCode: 500,
-                //         body: JSON.stringify(error)
-                //     }
-                // }
+                const snsInput = {
+                    Source: "rajeshros23@gmail.com",
+                    Destination: {
+                        ToAddresses: [email]
+                    },
+                    Message: {
+                        Body: {
+                            Text : {
+                                Data: message,
+                                Charset: "UTF-8"
+                            }
+                        },
+                        Subject : {
+                            Charset: "UTF-8",
+                            Data: "Expiring Items"
+                        }
+                    }
+                }
 
                 // now send expiring emails
                 try {
-                    const pubRes = await snsClient.send(new PublishCommand({
-                        Message: message,
-                        TopicArn: "arn:aws:sns:us-east-1:986376103464:ExpiringItems"
-                    }))
+                    const emailRes = await sesClient.send(new SendEmailCommand(snsInput))
 
                     return {
                         statusCode: 200,
-                        body: JSON.stringify(pubRes)
+                        body: JSON.stringify(emailRes)
                     }
                 } catch (error) {
                     console.error('SNS error:', error)
