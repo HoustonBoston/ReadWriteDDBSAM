521e6301a2f291e1e398afc60c262356ea07401b
HoustonBoston <myhorsefly12345@gmail.com>
2024-12-23 11:18:58 -0500

    added code to send email subscription verification if not already in the User table.

--- check-email-existence/check_email_existence.mjs
+++ check-email-existence/check_email_existence.mjs
@@ -2,15 +2,19 @@
  * Checks if given email exists in DB
  */
 
-import { GetItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import { GetItemCommand, DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb"
+import { SESClient, VerifyEmailIdentityCommand } from "@aws-sdk/client-ses"
 
-const client = new DynamoDBClient({})
+const dynamoClient = new DynamoDBClient({})
 const tableName = "User"
+const sesClient = new SESClient({})
 
-export const handler = async (event, context) => {
+export const handler = async (event, context) =>
+{
     const body = typeof (event.body) === 'string' ? JSON.parse(event.body) : event.body
     let userEmail = body?.email || ""
     const input = {
+        TableName: tableName,
         Key: {
             user_email: {
                 S: userEmail
@@ -20,9 +24,51 @@ export const handler = async (event, context) => {
 
     try {
         const command = new GetItemCommand(input)
-        const res = await client.send(command)
+        const getRes = await dynamoClient.send(command)
+        console.log("getRes.Item:", getRes.Item)
+
+        if (res.Item) // if it exists don't do anything
+            return {
+                statusCode: 200,
+                body: true
+            }
+        else { // otherwise send email subscription req and then put in DB
+            try {
+                const putRes = dynamoClient.send(new PutItemCommand({
+                    TableName: tableName,
+                    Item: {
+                        user_email: { S: userEmail }
+                    }
+                }))
+                console.log('putRes:', putRes)
+            } catch (e) {
+                console.error('Failed to put in DDB', e)
+                return {
+                    statusCode: 500,
+                    body: JSON.stringify(e)
+                }
+            }
+
+            // email sub req
+            try {
+                let verifyRes = sesClient.send(new VerifyEmailIdentityCommand({
+                    EmailAddress: userEmail
+                }))
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(verifyRes)
+                }
+            } catch (e) {
+                console.log("error when trying to send email sub req:", e)
+                return {
+                    statusCode: 500,
+                    body: JSON.stringify(e)
+                }
+            }
+        }
+
     } catch (e) {
-        console.error(e)
+        console.error("couldn't retrieve from DDB", e)
         return {
             statusCode: 500,
             body: JSON.stringify(e)
