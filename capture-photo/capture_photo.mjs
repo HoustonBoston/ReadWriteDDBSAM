13cf4314a4dee9c973b81f157e37cc9546243dc2
HoustonBoston <myhorsefly12345@gmail.com>
2025-01-18 13:50:41 -0500

    Switched from Amazon Rekognition to Moondream AI for now.

--- capture-photo/capture_photo.mjs
+++ capture-photo/capture_photo.mjs
@@ -4,19 +4,25 @@ const model = new vl({
     apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXlfaWQiOiJlMWNiMzhiMi1mMWQxLTRlYjctOGMzYS02MWNhOTViNmZhMGQiLCJpYXQiOjE3MzcxMzc4OTR9.BI0FT3zvPi_aXXbePnj_K05P8j2WR1_hQ7mTmhoAaLo"
 })
 
-export const handler = async (event) => {
+export const handler = async (event) =>
+{
     console.log('entering capture photo lambda function')
+
     const body = typeof (event.body) === 'string' ? JSON.parse(event.body) : event.body
 
-    let image = body?.base64Image || "" //image is a base64 encoded string
+    let img = body?.base64Image || "" //image is a base64 encoded string
+    if (img.startsWith("data:image")) img = img.split(",")[1] // removes the "data:image/*" part AKA MIME prefix
+    img = Buffer.from(img, "base64")
 
     try {
+        console.log('trying to query moondream')
+
         const response = await model.query({
-            image: image,
-            question: "What is the expiry date on this object?"
+            image: img,
+            question: "What is the expiry date on this object? Give me the date in USA format. No full answer, date only."
         })
 
-        console.log('answer:', response)
+        console.log('response:', JSON.stringify(response))
 
         return {
             statusCode: 200,
@@ -28,10 +34,10 @@ export const handler = async (event) => {
             }
         }
     } catch (e) {
-        console.error('error when calling moondream:', e)
+        console.error('error in capture photo lambda function!', e)
 
         return {
-            statusCode: 200,
+            statusCode: 500,
             body: JSON.stringify(e),
             headers: {
                 "Access-Control-Allow-Headers": "Content-Type",
